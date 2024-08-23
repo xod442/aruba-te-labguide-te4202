737d9be9a0b4672604e57235dd8a40bf89cfca09
Rick Kauffman <rick@rickkauffman.com>
2024-08-23 10:46:08 -0400

    see file for any changes

--- gen_te4202.sh
+++ gen_te4202.sh
@@ -5,4 +5,4 @@ git commit -m 'see file for any changes'
 git push origin main
 
 jb build $HOME/opt/lab-guides/datacenter/aruba-te-labguide-te4202/docs/
-sudo cp -R $HOME/opt/lab-guides/datacenter/aruba-te-labguide-te4202/docs/_build/html/* /var/www/html/te4201/
+sudo cp -R $HOME/opt/lab-guides/datacenter/aruba-te-labguide-te4202/docs/_build/html/* /var/www/html/te4202/
